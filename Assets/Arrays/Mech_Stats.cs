a3bcfc56d5e691ba4ec4f4dcd856cb192cad6fcf
haladrias <haladrias@gmail.com>
2024-09-09 19:54:47 -0700

    first run at stats array

---
+++ Assets/Arrays/Mech_Stats.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mech_Stats : MonoBehaviour
+{ public string[ 
+    HP,  //Hit Points of Mech
+    Armor, //Damage reduction
+    Mov,  //number of squares/tiles unit can move
+    Avo, //percentage chance to hit reduction
+    Acc,  //percentage chance to hit
+    Damage, //damage dealt by weapon
+    ]Mech_Stats; 
+    }
