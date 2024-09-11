ecdb2d422e6ead9f606b113af6278abc7b955dfc
Paul Adrian <67833935+alcoranpaul@users.noreply.github.com>
2024-09-11 14:35:51 -0500

    Update GridTesting.cs

--- Assets/Scripts/GridSystem/GridTesting.cs
+++ Assets/Scripts/GridSystem/GridTesting.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Testing purposes for the grid system
+/// </summary>
 public class GridTesting : MonoBehaviour
 {
 
